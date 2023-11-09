f6f50197af99734e2812e0eda4e2dce05cd1e27f
rlawogks22 <rlawogks1234@gmail.com>
2023-11-09 17:36:38 +0900

    알고리즘 테스트 정수 자릿수 더하기

--- src/level1/test1.java
+++ src/level1/test1.java
@@ -1,2 +1,17 @@
-package level1;public class test1 {
+package level1;
+
+//정수 자릿수 더하기
+public class test1 {
+    public static void main(String[] args) {
+        int n = 123;
+        int an = 0 ;
+        String st = String.valueOf(n); // 정수 물자열 변환
+        for(int i = 0; i < st.length(); i++){
+            an += Integer.parseInt(String.valueOf(st.charAt(i))); // 물자열을 정수로 변환해 한단어 씩 더하기
+        }
+        System.out.println(an);
+
+    }
 }
+
+
