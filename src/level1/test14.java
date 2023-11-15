dbba8d78b170b8bd8427832c2766ea36aae4d743
rlawogks22 <rlawogks1234@gmail.com>
2023-11-15 09:54:22 +0900

    알고리즘 테스트 //음양 더하기

--- src/level1/test14.java
+++ src/level1/test14.java
@@ -2,15 +2,17 @@ package level1;
 
 public class test14 {
     public static void main(String[] args) {
-        int an = 123456789;
-        int[] ab = {4,7,12};
-        boolean[] si = {true,false,true};
+        int an = 0;
+        int[] ab = {4, 7, 12};
+        boolean[] si = {true, false, true};
 
-        for(int i = 0; i < ab.length; i++){
-            if(si[i] == true){
-
+        for (int i = 0; i < si.length; i++) {
+            if (si[i] == true) {
+                an += ab[i];
+            } else {
+                an -= ab[i];
             }
         }
-
+        System.out.println(an);
     }
 }
