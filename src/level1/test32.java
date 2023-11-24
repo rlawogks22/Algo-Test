3e7d9dabe2bac8faef7a1cd6df351a8135a6c8df
rlawogks22 <rlawogks1234@gmail.com>
2023-11-24 09:16:52 +0900

    알고리즘 테스트 //최소 직사강형

--- src/level1/test32.java
+++ src/level1/test32.java
@@ -6,14 +6,15 @@ public class test32 {
         int an = 0;
         int max_w = 0;
         int max_h = 0;
+        String a = "";
+        String b = "";
 
         for(int i = 0; i<size.length; i++){
             int w = Math.max(size[i][0],size[i][1]); //w 가로 두 변 중 가장 긴 부분
             int h = Math.min(size[i][0],size[i][1]); //h 세로 두 변 중 가장 작은 부분
             max_w = Math.max(max_w,w);
             max_h = Math.max(max_h,h);
-        }
-        an = max_w*max_h;
+            }
         System.out.println(an);
     }
 }
